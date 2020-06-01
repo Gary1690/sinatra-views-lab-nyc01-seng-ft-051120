f797f38e9013fb5140f44f18e6d12f4173cc6311
Gary Cordero <GaryCordero1690@gmail.com>
2020-06-01 18:34:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello 
+	end
 
+	get '/goodbye' do 
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do 
+		@date = DateTime.now.strftime("%A, %B %d, 2020")
+		erb :date
+	end
 end
